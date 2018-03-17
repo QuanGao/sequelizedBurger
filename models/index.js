44ed501fa2b47c795d321ed2b35ed54afb389b17
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-03-16 22:45:28 -0500

    edit index.js2

--- models/index.js
+++ models/index.js
@@ -8,8 +8,14 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     match[4],
+    host:     match[3],
+    logging:  true
+  })
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
